7d195b04eed94548797ea0a95fab76f91b0fe080
John VanDyke-Mensah Jr <126047612+jvandykemen@users.noreply.github.com>
2024-10-20 14:43:37 -0400

    Person Class
    Person class made according to class diagrams

---
+++ src/main/java/edu/gmu/cs321/Dependant.java
@@ -0,0 +1,9 @@
+package edu.gmu.cs321;
+public class Dependant extends Immigrant{
+
+    public Dependant(String firstName) {
+        super(firstName);
+        //TODO Auto-generated constructor stub
+    }
+
+}
--- src/main/java/edu/gmu/cs321/Person.java
+++ src/main/java/edu/gmu/cs321/Person.java
@@ -2,8 +2,22 @@ package edu.gmu.cs321;
 
 public class Person 
 {
-	String firstName;
-    Person(String firstName){
-		this.firstName = firstName;
+	private String firstName;
+	private String lastName;
+	private int personID;
+	public Person(String firstName, String lastName, int personID){
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.personID=personID;
 	}
+	public String getFirstName(){
+		return firstName;
+	}
+	public String getLastName(){
+		return lastName;
+	}
+	public int getID(){
+		return personID;
+	}
+	
 }
