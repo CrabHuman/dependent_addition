29ebf2e03ef066308c9656123d8f0520fad3fb6e
CrabHuman <60482547+CrabHuman@users.noreply.github.com>
2024-10-19 20:38:45 -0400

    test4

---
+++ src/main/java/edu/gmu/cs321/Immigrant.java
@@ -0,0 +1,9 @@
+package edu.gmu.cs321;
+
+public class Immigrant extends Person{
+
+    public Immigrant(String firstName){
+        //this.firstName = firstName;
+        super(firstName);
+    }
+}
