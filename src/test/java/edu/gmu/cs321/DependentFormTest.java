bac45d85ad08d53a551432ab5e04d0461f16b367
jwilbur3 <jackwilbur2017@gmail.com>
2024-10-20 16:04:34 -0400

    DependentFormTest added

---
+++ src/test/java/edu/gmu/cs321/DependentFormTest.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DependentFormTest
+{
+    @Test
+    public void DependentFormConstructor()
+    {
+        DependentForm df = new DependentForm();
+        
+    }
+}
