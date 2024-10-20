a061a3150dcc364069f698a87048337ca18989ad
jwilbur3 <jackwilbur2017@gmail.com>
2024-10-20 15:00:43 -0400

    Stage.java initial content

--- src/main/java/edu/gmu/cs321/Stage.java
+++ src/main/java/edu/gmu/cs321/Stage.java
@@ -1,5 +1,26 @@
 package edu.gmu.cs321;
 
 public class Stage {
-    
+    private DependantForm form;
+
+    public Stage()
+    {
+        this.form = null;
+    }
+
+    public DependantForm getDependantForm()
+    {
+        return this.form;
+    }
+
+    public int nextForm()
+    {
+        //fetch next DependantForm from Workflow
+        return 1;
+    }
+
+    public int saveForm()
+    {
+        return 1;
+    }
 }
