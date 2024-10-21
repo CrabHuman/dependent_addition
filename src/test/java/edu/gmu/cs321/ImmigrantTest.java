0a69ecb0ba0028ef971f9251ffa987f29d11ffe7
CrabHuman <60482547+CrabHuman@users.noreply.github.com>
2024-10-20 20:51:02 -0400

    Immigrant junit tests

--- src/test/java/edu/gmu/cs321/ImmigrantTest.java
+++ src/test/java/edu/gmu/cs321/ImmigrantTest.java
@@ -1,29 +1,81 @@
 package edu.gmu.cs321;
-// import java.util.Date;
+import java.util.Date;
 import java.util.LinkedList;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ImmigrantTest{
-    @Test
-    public 
-     void TestImmigrant(){
-        Immigrant imm = new Immigrant();
-        //assertFalse(imm.hasDependents());
-        // }else{
-        //     assertEquals(tr,true);
-        //     System.out.println("Test 1 Immigrant Test not successful");
-        // }
+    static Immigrant immigrant;
+    static Immigrant empty;
+    static Immigrant fill;
 
-        // Date dob = new Date();
-        // Immigrant imm  = new Immigrant("Lonzo","Ball",02,dob);
-        
-        // assertFalse(imm.hasDependents());
-
-        // imm.addDependent(new);
+    @BeforeAll
+    static void setUp(){
+        immigrant = new Immigrant("Bob", "Bryant", 655, new Date(1000000000L), "Courtlane Dr", 1112223333L, "bb@b.com" );
+        empty = new Immigrant();
+        fill = new Immigrant("Carl", "Bumps",882);
     }
+    
+    @Test
+    void DoBTester(){
+        assertEquals(new Date(1000000000L),immigrant.getDateOfBirth());
+    }
+    @Test
+    void addressTester(){
+        assertEquals("Courtlane Dr",immigrant.getAddress());
+    }
+    @Test
+    void phoneNumberTester(){
+        assertEquals(1112223333L, immigrant.getPhoneNumber());
+    }
+    @Test
+    void emailTester(){
+        assertEquals("bb@b.com", immigrant.getEmail());
+    }
+
+    @Test
+    void defaultDoBTester(){
+        assertNull(empty.getDateOfBirth());
+    }
+    @Test
+    void defaultAddressTester(){
+        assertNull(empty.getAddress());
+    }
+    @Test
+    void defaultPhoneNumberTester(){
+        assertEquals(-1,empty.getPhoneNumber());
+    }
+    @Test
+    void defaultEmailTester(){
+        assertNull(empty.getEmail());
+    }
+
+    @Test
+    void setDoBTester(){
+        fill.setDateOfBirth(new Date(2000L));
+        assertEquals(new Date(2000L),fill.getDateOfBirth());
+    }
+    @Test
+    void setAddressTester(){
+        fill.setAddress("Terraceloop Rd");
+        assertEquals("Terraceloop Rd",fill.getAddress());
+    }
+    @Test
+    void setPhoneNumberTester(){
+        fill.setPhoneNumber(9998887777L);
+        assertEquals(9998887777L,fill.getPhoneNumber());
+    }
+    @Test
+    void setEmailTester(){
+        fill.setEmail("dan@dan.com");
+        assertEquals("dan@dan.com",fill.getEmail());
+    }
+
 }
--- src/test/java/edu/gmu/cs321/PersonTest.java
+++ src/test/java/edu/gmu/cs321/PersonTest.java
@@ -2,11 +2,8 @@ package edu.gmu.cs321;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import org.junit.jupiter.api.BeforeAll;
 //import org.junit.jupiter.api.BeforeEach;
