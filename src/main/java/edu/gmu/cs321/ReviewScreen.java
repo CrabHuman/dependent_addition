91929043eba690abeabe102b342a075c75d89e6b
CrabHuman <60482547+CrabHuman@users.noreply.github.com>
2024-11-02 13:05:30 -0400

    fxml splitting

---
+++ src/main/java/edu/gmu/cs321/ReviewScreen.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ReviewScreen extends Screen {
+
+    @FXML
+    private void testButtonMethod() throws IOException {
+        
+        App.setRoot("primary");
+    }
+}
