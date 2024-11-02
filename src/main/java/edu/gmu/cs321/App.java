1e283170dd349deacab122dcc595d37fd77f3c97
John VanDyke-Mensah Jr <126047612+jvandykemen@users.noreply.github.com>
2024-11-02 13:49:25 -0400

    Data Entry Seperate Screen Creation

--- src/main/java/edu/gmu/cs321/App.java
+++ src/main/java/edu/gmu/cs321/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,18 +16,32 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Scene scene2;
 
+    
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        
+        //Seperate Screen For Data Entry
+        scene2=new Scene(creation(),300,300);
+        Stage destage=new Stage();
+        destage.setTitle("Data Entry");
+        destage.setScene(scene2);
+        destage.show();
+        
     }
+    
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
+    //creation method for data entry
+    private static Parent creation(){
+        return new StackPane(new Text("Data Entry Main Screen"));
+    }
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
---
+++ src/main/java/edu/gmu/cs321/CreateDE.java
@@ -0,0 +1,7 @@
+package edu.gmu.cs321;
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class CreateDE {
+    
+}
