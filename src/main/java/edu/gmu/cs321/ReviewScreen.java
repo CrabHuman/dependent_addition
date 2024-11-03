bb47f33d5f0e87224dfb755025788ff7ff27d18f
jwilbur3 <jackwilbur2017@gmail.com>
2024-11-03 09:03:41 -0500

    Review Screen Prototype Looking Passable

--- src/main/java/edu/gmu/cs321/ReviewScreen.java
+++ src/main/java/edu/gmu/cs321/ReviewScreen.java
@@ -6,7 +6,31 @@ import javafx.fxml.FXML;
 public class ReviewScreen extends Screen {
 
     @FXML
-    private void testButtonMethod() throws IOException {
+    private void saveForm() throws IOException {
+        
+        App.setRoot("primary");
+    }
+    
+    @FXML
+    private void submitForm() throws IOException {
+        
+        App.setRoot("primary");
+    }
+
+    @FXML
+    private void backToPrimary() throws IOException {
+        
+        App.setRoot("primary");
+    }
+
+    @FXML
+    private void pauseForm() throws IOException {
+        
+        App.setRoot("primary");
+    }
+
+    @FXML
+    private void loadNextForm() throws IOException {
         
         App.setRoot("primary");
     }
