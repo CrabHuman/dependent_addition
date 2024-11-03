87877a857b241bd5e0e6ebc4f4168beaf66af391
jwilbur3 <jackwilbur2017@gmail.com>
2024-11-03 10:36:25 -0500

    ReviewScreen.java & review.fxml comments

--- src/main/java/edu/gmu/cs321/ReviewScreen.java
+++ src/main/java/edu/gmu/cs321/ReviewScreen.java
@@ -7,31 +7,38 @@ public class ReviewScreen extends Screen {
 
     @FXML
     private void saveForm() throws IOException {
-        
-        App.setRoot("primary");
+        // save the form to the database
     }
     
     @FXML
     private void submitForm() throws IOException {
-        
-        App.setRoot("primary");
+        // save form to database
+        saveForm();
+        // send form into workflow table
+        // unload this form from the ReviewScreen
     }
 
+    // renamed back to testButtonMethod from backToPrimary for basic functionality before I figure out how it works.
     @FXML
-    private void backToPrimary() throws IOException {
-        
+    private void testButtonMethod() throws IOException {
+        // save form to database
+        saveForm();
+        // go back to previous page
         App.setRoot("primary");
     }
 
     @FXML
     private void pauseForm() throws IOException {
-        
-        App.setRoot("primary");
+        // save form to database (maybe mark as a draft or something)
+        saveForm(/*DependentForm.draft=true*/);
+        // keep form loaded
     }
 
     @FXML
     private void loadNextForm() throws IOException {
-        
-        App.setRoot("primary");
+        // save form to database
+        saveForm();
+        // unload this form
+        // retrieve new form from the database
     }
 }
