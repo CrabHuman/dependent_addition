22afa5f6d05a02254347ec66232b1a948677ca62
John VanDyke-Mensah Jr <126047612+jvandykemen@users.noreply.github.com>
2024-11-04 17:59:10 -0500

    Changes to Screen

--- src/main/java/edu/gmu/cs321/DataEntryScreen.java
+++ src/main/java/edu/gmu/cs321/DataEntryScreen.java
@@ -1,13 +1,15 @@
 package edu.gmu.cs321;
-
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class DataEntryScreen extends Screen {
-
     @FXML
     private void testButtonMethod() throws IOException {
-        
         App.setRoot("primary");
+    }
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) throws IOException {
+        App.setRoot("main");  
     }
 }
